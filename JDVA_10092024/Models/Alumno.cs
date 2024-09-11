14610b42ac712afd6e87ef33832005d97d7ac0b5
Vasquez8 <dv113409@gmail.com>
2024-09-10 23:35:39 -0600

    Commit inicial

---
+++ JDVA_10092024/Models/Alumno.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JDVA_10092024.Models;
+
+public class Alumno
+{
+    public int Id {get; set;}
+    public string Nombre {get; set;}
+    public string Apellido {get; set;}
+    public string Codigo {get; set;}
+}
